b5537eeb4b04a1671f7cdd7e62dc4671dff045e3
Jürgen Anke <juergen.anke@htw-dresden.de>
2025-01-02 21:50:04 +0100

    added json-server and access to endpoint via axios in useEffect hook.

--- src/App.jsx
+++ src/App.jsx
@@ -1,22 +1,39 @@
 import Note from "./components/Note";
-import { useState } from "react";
+import { useState, useEffect } from "react";
+import axios from "axios";
 
-const App = (props) => {
-  const [notes, setNotes] = useState(props.notes);
+const App = () => {
+  const [notes, setNotes] = useState([]);
   const [newNote, setNewNote] = useState("a new note...");
-  const [showAll, setShowAll] = useState(true)
+  const [showAll, setShowAll] = useState(true);
+
+  //this function is executed after the initial rendering of the component
+  const hook = () => {
+    console.log("effect");
+    axios.get("http://localhost:3001/notes").then((response) => {
+      console.log("promise fulfilled");
+      setNotes(response.data); //this state change triggers a re-rendering of the component
+    });
+  };
+
+  //second parameter defines the reactive values whose change will trigger the effect
+  // /specifying an empty array results in a single run (at the initial rendering)
+  useEffect(hook, []);
+
+  //first rendering: 0 notes, second rendering: 3 notes
+  console.log("render", notes.length, "notes");
 
   const addNote = (event) => {
     event.preventDefault(); //prevents submitting the form, which causes a page reload
-    
+
     const noteObject = {
       content: newNote,
       important: Math.random() < 0.5,
       id: String(notes.length + 1),
     };
 
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+    setNotes(notes.concat(noteObject));
+    setNewNote("");
   };
 
   const handleNoteChange = (event) => {
@@ -24,12 +41,16 @@ const App = (props) => {
     setNewNote(event.target.value);
   };
 
-  const notesToShow = showAll ? notes : notes.filter(note => note.important)
+  const notesToShow = showAll ? notes : notes.filter((note) => note.important);
 
   return (
     <div>
       <h1>Notes</h1>
-      <div><button onClick={() => setShowAll(!showAll)}>show {showAll ? 'important' : 'all'}</button></div>
+      <div>
+        <button onClick={() => setShowAll(!showAll)}>
+          show {showAll ? "important" : "all"}
+        </button>
+      </div>
       <ul>
         {notesToShow.map((note) => (
           <Note key={note.id} note={note} />
